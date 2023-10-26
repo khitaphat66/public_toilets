1c90a1b39ac14626d95ed8baddd16958c7450201
Khitaphat Kaeophonngam <kaeophonngam_k@silpakorn.edu>
2023-10-26 08:49:45 +0700

    Initial commit

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:public_toilets/models/toilet.dart';
+import 'package:public_toilets/repositories/toilet_repository.dart';
+import 'package:public_toilets/screens/home/add_toilet.dart';
+import 'package:public_toilets/screens/home/toilet_list_item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Toilet>? _toilets;
+  var _isLoading = false;
+  String? _errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    getToilets();
+  }
+
+  getToilets() async {
+    setState(() {
+      _isLoading = true;
+      _errorMessage = null;
+    });
+
+    await Future.delayed(Duration(seconds: 2));
+
+    try {
+      var toilets = await ToiletRepository().getToilets();
+      debugPrint('Number of toilets: ${toilets.length}');
+
+      setState(() {
+        _toilets = toilets;
+      });
+    } catch (e) {
+      setState(() {
+        _errorMessage = e.toString();
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    buildLoadingOverlay() => Container(
+        color: Colors.black.withOpacity(0.2),
+        child: Center(child: CircularProgressIndicator()));
+
+    buildError() => Center(
+        child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child:
+            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(_errorMessage ?? '', textAlign: TextAlign.center),
+              SizedBox(height: 32.0),
+              ElevatedButton(onPressed: getToilets, child: Text('Retry'))
+            ])));
+
+    buildList() => ListView.builder(
+        itemCount: _toilets!.length,
+        itemBuilder: (ctx, i) {
+          Toilet toilet = _toilets![i];
+          return ToiletListItem(toilet: toilet);
+        });
+
+    handleClickAdd() {
+      Navigator.pushNamed(context, AddToiletPage.routeName).whenComplete(() {
+        getToilets();
+      });
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Public Toilets'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: handleClickAdd, child: Icon(Icons.add)),
+        body: Stack(
+          children: [
+            if (_toilets?.isNotEmpty ?? false) buildList(),
+            if (_errorMessage != null) buildError(),
+            if (_isLoading) buildLoadingOverlay()
+          ],
+        ));
+  }
+}
---
+++ lib/screens/home/toilet_list_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:public_toilets/models/toilet.dart';
+
+class ToiletListItem extends StatelessWidget {
+  static const iconSize = 18.0;
+
+  final Toilet toilet;
+
+  const ToiletListItem({
+    super.key,
+    required this.toilet,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    var colorScheme = Theme.of(context).colorScheme;
+
+    var hasRating = toilet.averageRating > 0;
+    var numWholeStar = toilet.averageRating.truncate();
+    var fraction = toilet.averageRating - numWholeStar;
+    var showHalfStar = fraction >= 0.5;
+    var numBlankStar = 5 - numWholeStar - (showHalfStar ? 1 : 0);
+
+    return Card(
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(children: [
+              Container(
+                  width: 60.0,
+                  height: 60.0,
+                  color: colorScheme.background,
+                  child: Center(child: Icon(Icons.home, size: 30.0))),
+              SizedBox(width: 8.0),
+              Expanded(child: Text(toilet.name, style: textTheme.titleLarge)),
+              Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                hasRating
+                    ? Row(children: [
+                  for (var i = 0; i < numWholeStar; i++)
+                    Icon(Icons.star, size: iconSize),
+                  if (showHalfStar) Icon(Icons.star_half, size: iconSize),
+                  for (var i = 0; i < numBlankStar; i++)
+                    Icon(Icons.star_border, size: iconSize),
+                  //Text(toilet.averageRating.toStringAsFixed(1))
+                ])
+                    : Text('ยังไม่มีคะแนน'),
+                Text('${toilet.distance.toString()} เมตร',
+                    style: textTheme.bodyLarge)
+              ])
+            ])));
+  }
+}
